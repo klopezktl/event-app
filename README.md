08891e03abcfd3ab1cd772f8747b299012519b46
Kimberly Lopez <klopezktl@gmail.com>
2022-12-28 21:32:18 +0800

    - Change data to dictionary - Add handling for dictionary - Add requirements

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # event-app
 Event App
 
+pip install -r requirements.txt
 export FLASK_RUN_PORT=8080
 docker build -t event-app .
 docker run -p 8080:8080 event-app
+
+pip install -r requirements.txt
--- app.py
+++ app.py
@@ -1,60 +1,43 @@
 from flask import Flask, request
+from db import events
+import uuid
 
 app = Flask(__name__)
-
-events = [
-    {
-        "event_code": "my_event_1",
-        "event_name": "My event 1",
-        "start_date": "2023-01-01",
-        "end_date": "2023-01-05"
-    },
-    {
-        "event_code": "my_event_2",
-        "event_name": "My event 2",
-        "start_date": "2023-01-06",
-        "end_date": "2023-01-07"
-    }
-]
 
 # List
 @app.get("/event")
 def get_events():
+    # return {"events": list(events.values())}
     return {"events": events}
 
 # Create
 @app.post("/event")
 def create_event():
-    request_data = request.get_json()
-    new_event = {
-        "event_code": request_data['event_code'],
-        "event_name": request_data["event_name"],
-        "start_date": request_data["start_date"],
-        "end_date": request_data["end_date"]
-    }
-
-    events.append(new_event)
-    return new_event, 201
+    event_data = request.get_json()
+    event_id = uuid.uuid4().hex
+    event = {**event_data, "id": event_id}
+    events[event_id] = event
+    return event, 201
 
 # Update
-@app.put("/event/<string:event_code>")
-def update_event(event_code):
+@app.put("/event/<string:event_id>")
+def update_event(event_id):
     request_data = request.get_json()
-    for event in events:
-        if event['event_code'] == event_code:
-            event['event_name'] = request_data['event_name']
-            event['start_date'] = request_data['start_date']
-            event['end_date'] = request_data['end_date']
-            return events, 201
+    if event_id not in events:
+        return {"message": "Event not found"}, 404
 
-    return {"message": "Event not found"}, 404
+    event = events.get(event_id)
+    event['event_name'] = request_data['event_name']
+    event['start_date'] = request_data['start_date']
+    event['end_date'] = request_data['end_date']
+
+    return event, 201
 
 # Delete
-@app.post("/event/<string:event_code>")
-def delete_event(event_code):
-    for i in range(len(events)):
-        if events[i]['event_code'] == event_code:
-            del events[i]
-            return events, 201
-
-    return {"message": "Event not found"}, 404
+@app.post("/event/<string:event_id>")
+def delete_event(event_id):
+    try:
+        del events[event_id]
+        return events, 201
+    except KeyError:
+        return {"message": "Event not found"}, 404
