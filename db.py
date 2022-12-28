67ece4f89cc2fd9681ae691152dc5b351eda5e1a
Kimberly Lopez <klopezktl@gmail.com>
2022-12-28 21:31:32 +0800

    - Change data to dictionary - Add handling for dictionary - Add requirements

---
+++ db.py
@@ -0,0 +1,12 @@
+events = {
+    "1": {
+        "event_name": "My event 1",
+        "start_date": "2023-01-01",
+        "end_date": "2023-01-05"
+    },
+    "2": {
+        "event_name": "My event 2",
+        "start_date": "2023-01-06",
+        "end_date": "2023-01-07"
+    }
+}
