4304f6cbc01cc2a2c079b1cad06b71ac62e5fe28
Kimberly Lopez <klopezktl@gmail.com>
2022-12-29 10:06:23 +0800

    Add components

--- event-web/src/app/app.module.ts
+++ event-web/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddEventComponent } from './add-event/add-event.component';
+import { EditEventComponent } from './edit-event/edit-event.component';
+import { DeleteEventComponent } from './delete-event/delete-event.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddEventComponent,
+    EditEventComponent,
+    DeleteEventComponent
   ],
   imports: [
     BrowserModule,
