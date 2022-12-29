d613239c47e246b1793ebed49580983dc3999ca0
Kimberly Lopez <klopezktl@gmail.com>
2022-12-29 10:17:17 +0800

    Additional changes to http-provider and web-api

--- event-web/src/app/Service/http-provider.service.ts
+++ event-web/src/app/Service/http-provider.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { WebApiService } from './web-api.service';
 
+var apiUrl = "http://localhost:8080/";
+
+var httpLink = {
+  getAllEvent: apiUrl + "/event",
+  deleteEventById: apiUrl + "/event",
+  updateEventById: apiUrl + "/event",
+  saveEvent: apiUrl + "/event"
+}
 @Injectable({
   providedIn: 'root'
 })
 export class HttpProviderService {
+  constructor(private webApiService: WebApiService) { }
 
-  constructor() { }
+  public getAllEvent(): Observable<any> {
+    return this.webApiService.get(httpLink.getAllEvent);
+  }
+  public deleteEventById(model: any): Observable<any> {
+    return this.webApiService.delete(httpLink.deleteEventById + '?event_id=' + model);
+  }
+  public updateEventById(model: any): Observable<any> {
+    return this.webApiService.put(httpLink.updateEventById + '?event_id=' + model, '');
+  }
+  public saveEvent(model: any): Observable<any> {
+    return this.webApiService.post(httpLink.saveEvent, model);
+  }
 }
