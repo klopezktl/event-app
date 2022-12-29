2f3327cd78a898f909c044dab64cd7ec8b4bea87
Kimberly Lopez <klopezktl@gmail.com>
2022-12-29 10:08:20 +0800

    Add routing

--- event-web/src/app/app-routing.module.ts
+++ event-web/src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
-@NgModule({
+import { AddEventComponent } from './add-event/add-event.component';
+import { EditEventComponent } from './edit-event/edit-event.component';
+import { HomeComponent } from './home/home.component';
+import { DeleteEventComponent } from './delete-event/delete-event.component';\
+const routes: Routes = [
+  { path: '', redirectTo: 'Home', pathMatch: 'full'},
+  { path: 'Home', component: HomeComponent },
+  { path: 'DeleteEvent/:eventId', component: DeleteEventComponent },
+  { path: 'AddEvent', component: AddEventComponent },
+  { path: 'EditEvent/:eventId', component: EditEventComponent }
+];
+ @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
